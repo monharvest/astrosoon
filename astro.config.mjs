a0ef7ef85cb54f2276f8b4cc7a940a0d48357c51
monharvest <monharvest@gmail.com>
2025-11-07 13:07:22 +0100

    Add robots.txt and update site URL for SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://astrosoon.pages.dev',
   integrations: [tailwind(), sitemap()]
 });
