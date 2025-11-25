f7870f60df26e0105ab2cee123e0b45a5ce82e09
monharvest <monharvest@gmail.com>
2025-11-25 14:38:51 +0100

    Remove Decap CMS and Netlify Identity integration

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,10 +1,3 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="vite-plugin-pwa/client" />
 /// <reference types="vite-plugin-pwa/info" />
-
-interface Window {
-  netlifyIdentity?: {
-    on: (event: string, callback: (user?: any) => void) => void;
-    init: () => void;
-  };
-}
