e8c7edb9ab021722f05fd2f40e664c25ae1c8b02
monharvest <monharvest@gmail.com>
2026-02-27 14:20:15 +0100

    Remove advent category and images

--- src/utils/categoryStyles.ts
+++ src/utils/categoryStyles.ts
@@ -1,5 +1,4 @@
 export const categoryClasses: Record<string, string> = {
-  Advent: 'from-gray-500 to-gray-600',
   'Сургаалт зүйрлэлүүд': 'from-gray-400 to-gray-500',
   'Үхэл ба амилал': 'from-gray-600 to-gray-700',
   'Мөнх үгийн ойлголт': 'from-gray-500 to-gray-600',
